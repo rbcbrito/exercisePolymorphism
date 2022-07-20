24d36099abd3721622f4f2c5e446978000348f96
Roberto Bruno <robertobcb@gmail.com>
2022-07-20 20:01:32 -0300

    Adicionado Classes Employee, OutsourcedEmployee

---
+++ src/entities/OutsourcedEmployee.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+
+	private Double additionalCharge;
+
+	public OutsourcedEmployee() {
+		super();
+	}
+
+	public OutsourcedEmployee(String name, Integer hours, Double valuePerHour, Double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public Double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+
+}
